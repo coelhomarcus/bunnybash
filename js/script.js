f609eb8c5bafdbdedb1fe7fe22e37af4291ab4dd
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-02-14 17:18:09 -0300

    update: text input

--- js/script.js
+++ js/script.js
@@ -1,128 +1,119 @@
 import messages from './modules/messages.js';
 import useTheme from './modules/themes.js';
 
-const consoleText = document.querySelector(".text");
+const input = document.querySelector("#text");
 const username = document.querySelector(".username");
 const divider = document.querySelector(".divider");
 const path = document.querySelector(".path");
 const pathDefault = " ";
 const answers = document.querySelector(".answers");
 
+input.value = "";
+
 document.addEventListener("keydown", type);
 function type(e) {
 
-
-    const isLetter = /^.$/.test(e.key);
-    if (isLetter) {
-        consoleText.innerText += e.key;
-    }
-
-    if (e.key == " ") {
-        consoleText.innerHTML += `<span>&nbsp</span>`
-        e.preventDefault();
-    }
-
-    if (e.key == "Backspace") {
-        consoleText.innerText = consoleText.innerText.slice(0, -1);
+    if (document.activeElement !== input) {
+        input.focus();
     }
 
     if (e.key == "Enter") {
 
         e.preventDefault();
 
-        if (consoleText.innerHTML == "clear" || consoleText.innerHTML == "c" || consoleText.innerHTML == "cls") {
+        if (input.value == "clear" || input.value == "c" || input.value == "cls") {
             cleanCommand();
         }
 
-        else if ((consoleText.innerHTML == "help") || (consoleText.innerHTML == "h")) {
+        else if ((input.value == "help") || (input.value == "h")) {
             simpleCommand(messages.help);
         }
 
-        else if ((consoleText.innerHTML == "forfun") || (consoleText.innerHTML == "ff")) {
+        else if ((input.value == "forfun") || (input.value == "ff")) {
             simpleCommand(messages.forfun);
         }
 
-        else if ((consoleText.innerHTML == "shortcut") || (consoleText.innerHTML == "s")) {
+        else if ((input.value == "shortcut") || (input.value == "s")) {
             simpleCommand(messages.shortcut);
         }
 
-        else if (consoleText.innerHTML == "ee") {
+        else if (input.value == "ee") {
             simpleCommand(messages.easteregg);
         }
 
-        else if (consoleText.innerHTML == "marcus") {
+        else if (input.value == "marcus") {
             simpleCommand(messages.marcus);
         }
 
-        else if (consoleText.innerHTML == "skills") {
+        else if (input.value == "skills") {
             simpleCommand(messages.skills);
         }
 
-        else if (consoleText.innerHTML == "social") {
+        else if (input.value == "social") {
             simpleCommand(messages.social);
         }
 
-        else if (consoleText.innerHTML == "joke") {
+        else if (input.value == "joke") {
             fetchCommand(messages.jokes, "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart");
         }
 
-        else if (consoleText.innerHTML == "btc") {
+        else if (input.value == "btc") {
             fetchCommand(messages.btc, "https://blockchain.info/ticker");
         }
 
-        else if (consoleText.innerHTML == "dance") {
+        else if (input.value == "dance") {
             fetchCommand(messages.dance, "https://api.waifu.pics/sfw/dance");
         }
 
-        else if (consoleText.innerHTML == "cat") {
+        else if (input.value == "cat") {
             fetchCommand(messages.cat, "https://api.thecatapi.com/v1/images/search");
         }
 
-        else if (consoleText.innerHTML == "themes") {
+        else if (input.value == "themes") {
             simpleCommand(messages.themes);
         }
 
-        else if ((consoleText.innerHTML == "useTheme &nbsp;") || (consoleText.innerHTML == "useTheme") || (consoleText.innerHTML == "usetheme")) {
+        else if ((input.value == "useTheme ") || (input.value == "useTheme") || (input.value == "usetheme")) {
             simpleCommand(["Type the number", "example: useTheme 2"]);
         }
 
-        else if ((consoleText.innerHTML == "useTheme &nbsp;1") || (consoleText.innerHTML == "t1") || (consoleText.innerHTML == "usetheme &nbsp;1")) {
+        else if ((input.value == "useTheme 1") || (input.value == "t1") || (input.value == "usetheme 1")) {
             useTheme(1);
             simpleCommand(["Switched to the default theme"]);
         }
 
-        else if ((consoleText.innerHTML == "useTheme &nbsp;2") || (consoleText.innerHTML == "t2") || (consoleText.innerHTML == "usetheme &nbsp;2")) {
+        else if ((input.value == "useTheme 2") || (input.value == "t2") || (input.value == "usetheme 2")) {
             useTheme(2);
             simpleCommand(["Switched to the cyberpunk theme"]);
         }
 
-        else if ((consoleText.innerHTML == "useTheme &nbsp;3") || (consoleText.innerHTML == "t3") || (consoleText.innerHTML == "usetheme &nbsp;3")) {
+        else if ((input.value == "useTheme 3") || (input.value == "t3") || (input.value == "usetheme 3")) {
             useTheme(3);
             simpleCommand(["Switched to the cold theme"]);
         }
 
-        else if ((consoleText.innerHTML == "useTheme &nbsp;4") || (consoleText.innerHTML == "t4") || (consoleText.innerHTML == "usetheme &nbsp;4")) {
+        else if ((input.value == "useTheme 4") || (input.value == "t4") || (input.value == "usetheme 4")) {
             useTheme(4);
             simpleCommand(["Switched to the black/white theme"]);
         }
 
-        else if (consoleText.innerHTML == "ascii") {
+        else if (input.value == "ascii") {
             simpleCommand(messages.ascii);
         }
 
-        else if (consoleText.innerHTML == "is &nbsp;anyone &nbsp;there?") {
+        else if (input.value == "is anyone there?") {
             simpleCommand([`<img class="imgborderless" src="https://i.giphy.com/n3UBd63oVlQLC.webp">`]);
             useTheme(4);
         }
 
-        else if (consoleText.innerHTML == "berserk") {
+        else if (input.value == "berserk") {
             const randomNumber = (Math.random() * (138 - 1) + 1).toFixed();
             simpleCommand([`<a href="https://img.coelhomarcus.com/bash/berserk/berserk%20(${randomNumber}).png" target="_blank"><img class="imgmanga" src="https://img.coelhomarcus.com/bash/berserk/berserk%20(${randomNumber}).png"></a>`]);
         }
 
         else {
-            addUserLine(consoleText.innerText);
-            consoleText.innerText = "";
+            addUserLine(input.value);
+            input.value = "";
             addMessages(["This command does not exist."]);
         }
     }
@@ -132,7 +123,7 @@ function addUserLine(text) {
     const p = document.createElement("p");
     p.innerHTML = `${username.innerText}${path.innerText} <span class="divider">${divider.innerText}</span><span class="command">${text}</span>`;
     answers.appendChild(p);
-    scrollToBottom();
+    window.scrollTo(0, document.body.scrollHeight);
 }
 
 function addConsoleLine(text) {
@@ -141,10 +132,6 @@ function addConsoleLine(text) {
     p.classList.add("typing");
     p.classList.add("console");
     answers.appendChild(p);
-    scrollToBottom();
-}
-
-function scrollToBottom() {
     window.scrollTo(0, document.body.scrollHeight);
 }
 
@@ -152,12 +139,12 @@ function cleanCommand() {
     const tutorial = document.querySelector("p");
     tutorial.innerHTML = ``;
     answers.innerHTML = ``;
-    consoleText.innerText = "";
+    input.value = "";
 }
 
 function simpleCommand(array) {
-    addUserLine(consoleText.innerText);
-    consoleText.innerText = "";
+    addUserLine(input.value);
+    input.value = "";
     addMessages(array);
 }
 
@@ -171,8 +158,8 @@ async function addMessages(array) {
 }
 
 function fetchCommand(array, urlFetch) {
-    addUserLine(consoleText.innerText);
-    consoleText.innerText = "";
+    addUserLine(input.value);
+    input.value = "";
     addFetchMessages(array, urlFetch);
 }
 
